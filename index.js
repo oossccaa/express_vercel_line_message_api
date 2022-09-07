3505dba7cefb44249043bcb4f5888cc54e007735
tris.xyh <tris.xyhlink8tech.tw>
2022-09-07 18:01:38 +0800

    running

--- index.js
+++ index.js
@@ -4,4 +4,7 @@ const app = express();
 app.listen(80, () => {
   console.log(`Example app listening on port ${8088}`)
 })
+app.get('/',(req,res)=>{
+  res.send('Line message API is running!')
+})
 module.exports = app;
